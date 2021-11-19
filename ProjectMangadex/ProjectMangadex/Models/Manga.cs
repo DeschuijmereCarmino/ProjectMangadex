b374c0aa97e9094ffe99941b03af5e31ba196db7
Carmino <carmino.deschuijmere@student.howest.be>
2021-11-19 18:09:38 +0100

    Class Manga created

---
+++ ProjectMangadex/ProjectMangadex/Models/Manga.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ProjectMangadex.Models
+{
+    //definieren van een manga object
+    public class Manga
+    {
+        [JsonProperty(PropertyName = "id")]
+        public Guid Id { get; set; }
+
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+
+        public string Cover { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [JsonProperty(PropertyName = "relationships")]
+        public List<Relationship> Relationships { get; set; }
+
+        [JsonExtensionData]
+        private Dictionary<string, JToken> _extraJsonData = new Dictionary<string, JToken>();
+
+        [OnDeserialized]
+        private void ProcessExtraJSonData(StreamingContext context)
+        {
+            JToken jsonTitle = (JToken)_extraJsonData["attributes"]["title"];
+            JToken jsonDescription = (JToken)_extraJsonData["attributes"]["description"];
+
+            Title = (string)jsonTitle.SelectToken("en");
+            Description = (string)jsonDescription.SelectToken("en");
+        }
+    }
+
+    public class Relationship
+    {
+        [JsonProperty(PropertyName = "id")]
+        public Guid Id { get; set; }
+
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+    }
+}
